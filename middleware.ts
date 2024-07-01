83a52df6210474c56a407b6d9b4ea5399f23a4e6
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-07-01 16:03:39 -0300

    atualizando sistema de verificação de app

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,29 @@
 import type { NextRequest } from "next/server"
+import CryptoJS from 'crypto-js'
 
 export function middleware(req: NextRequest) {
   const electronToken = req.cookies.get('application')?.value
   const authToken = req.cookies.get('auth-token')?.value
+  const plaintext = process.env.APP_KEY
+  let electron = false
 
-  if (!authToken && electronToken && !req.nextUrl.pathname.startsWith('/login')) {
+  if (electronToken && plaintext) {
+    const decryptedKey = CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(electronToken));
+    
+    if (decryptedKey === plaintext) {
+      electron = true
+    }
+}
+
+  if (!authToken && electron && !req.nextUrl.pathname.startsWith('/login')) {
     return Response.redirect(new URL('/login', req.url))
   }
 
-  if (authToken && electronToken && !req.nextUrl.pathname.startsWith('/dashboard')) {
+  if (authToken && electron && !req.nextUrl.pathname.startsWith('/dashboard')) {
     return Response.redirect(new URL('/dashboard/overview/analytics', req.url))
   }
 
-  if (!electronToken && !req.nextUrl.pathname.startsWith('/not-application')) {
+  if (electron === false && !req.nextUrl.pathname.startsWith('/not-application')) {
     return Response.redirect(new URL('/not-application', req.url))
   }
 }
